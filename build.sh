09c0873e9f2e2bd403911dd61a65e3464a5594ab
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2023-04-12 21:18:07 -0300

    Binding generation rewritten to Teal language
    Teal it's a typed dialect of Lua, using Teal helps in finding typos, type mismatches and other common advantages of using a typed language, thus making the maintenance cost go lower at the price of a bit more complex setup (because Teal is now required)*.
    
    *Hopefully in the future a script will be available that'll download and compile a Lua and Teal runtimes locally, thus making setup easier.

--- build.sh
+++ build.sh
@@ -3,14 +3,25 @@
 # Please refer to the LICENSE file for details
 # SPDX-License-Identifier: Zlib
 
+# note: this script currently requires clang-check and tl commands.
+# clang-check: is part of clang tooling.
+# tl: the Teal language compiler.
+# You also will need to install a Lua runtime on your system (required by Teal).
+
 # usage: just use the command "$ sh build.sh"
-# you can also change the C compiler, Lua interpreter or object archiver by
-# passing parameters, like: "$ sh build.sh emcc nelua-lua emar"
-# note however for the time being only the defaults were tested.
+#
+# you can also change the C compiler or object archiver by
+# passing arguments, like: "$ sh build.sh emcc emar"
+#
+# note: currently only clang with llvm-ar, and emcc with emar are tested.
+
+# utils
+build_log() {
+  echo "nene build log: $1"
+}
 
 # variables
 CC=clang
-LUA=lua
 AR=llvm-ar
 
 if [ $# -gt 0 ] # C compiler passed
@@ -23,16 +34,9 @@ then
 AR=$2
 fi
 
-if [ $# -gt 2 ] # Lua interpreter passed
-then
-LUA=$3
-fi
-
-# echo configuration
-echo "config:"
-echo "C compiler: $CC"
-echo "Object achiver: $AR"
-echo "Lua interpreter: $LUA"
+# log configuration
+build_log "C compiler: $CC"
+build_log "Object achiver: $AR"
 
 # setup flags
 
@@ -49,7 +53,7 @@ IFLAGS="-I./include/"
 SOURCES="src/*.c src/math/*.c src/audio/*.c"
 
 # clear previous build
-echo "clear previous build"
+build_log "clear previous build"
 
 rm -rf build/
 mkdir build/
@@ -58,17 +62,17 @@ mkdir build/ast_dumps/math
 mkdir build/ast_dumps/audio
 
 # compile sources
-echo "creating object files"
+build_log "creating object files"
 
 $CC -c $WFLAGS $CSTD $IFLAGS $SOURCES
 mv *.o build/
 
 # archive objects for static linking
-echo "archiving object files"
+build_log "archiving object files"
 $AR -rcs build/libnene.a build/*.o
 
 # dump AST
-echo "dumping ASTs"
+build_log "dumping ASTs"
 dump_ast() {
   clang-check include/nene/$1.h -ast-dump -ast-dump-filter=$2 --extra-arg="-fno-color-diagnostics" > build/ast_dumps/$3.txt 
 }
@@ -93,8 +97,8 @@ dump_ast "tilemap" "nene_Tilemap" "tilemap"
 dump_ast "color" "nene_Color" "color"
 
 # generate bindings
-echo "generating bindings"
-echo "generating nelua bindings"
-$LUA bindgen/generate_nelua.lua
+build_log "generating bindings"
+build_log "generating nelua bindings"
+tl run bindgen/generate_nelua.tl
 
-echo "done"
+build_log "done"
---
+++ tlconfig.lua
@@ -0,0 +1,4 @@
+return {
+  gen_target="5.4",
+  gen_compat="off",
+}
