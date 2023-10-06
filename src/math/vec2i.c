d002cf98401d7a2a3d1c254bf55b165057908ca6
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2023-10-06 18:27:55 -0300

    vec2i: add documentation and clone method.
    The clone method is off-topic, but I decided to commit this here, also, note that it uses a pointer paramenter, but on nelua binding it's a copy, that's a bug to be fixed.

--- src/math/vec2i.c
+++ src/math/vec2i.c
@@ -12,6 +12,13 @@ nene_Vec2i nene_Vec2i_zero(void) {
   return (nene_Vec2i){ .x = 0, .y = 0 };
 }
 
+nene_Vec2i nene_Vec2i_clone(nene_Vec2i *v) {
+  if (v != NULL) {
+    return *v;
+  }
+  return nene_Vec2i_zero();
+}
+
 nene_Vec2i nene_Vec2i_one(void) {
   return (nene_Vec2i){ .x = 1, .y = 1 };
 }
